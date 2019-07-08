c31ef49502c690960a124d43323023cc226db8eb
ag1363 <ag1363@gmail.com>
2019-07-08 13:09:46 +0430

    change to class component & setState inputValue

--- basics/src/Components/UserSetting.js
+++ basics/src/Components/UserSetting.js
@@ -2,25 +2,35 @@ import React from 'react'
 import { UserConsumer } from './UserProvider'
 import UserMessage from './UserMessage'
 
+export default class UserSettings extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      inputValue: ''
+    }
+    this.setUsername = this.setUsername.bind(this)
+  }
 
+  setUsername (event) {
+    this.setState({ inputValue: event.target.value })
+  }
 
-export default function UserSettings () {
-  return (
-    <UserConsumer>
-      {(context) => (
-        <div>
-          <h2>Settings</h2>
-          <label htmlFor='username'>Username: </label>
-          <input
-            id='username'
-            type='text'
-            onChange={event => {
-              context.state.updateUsername(event.target.value)
-            }}
-          />
-          {/* <button onClick={event => { context.state.updateUsername(event.target.value) }} > send </button> */}
-        </div>
-      )}
-    </UserConsumer>
-  )
+  render () {
+    return (
+      <UserConsumer>
+        {(context) => (
+          <div>
+            <h2>Settings</h2>
+            <label htmlFor='username'>Username: </label>
+            <input
+              id='username'
+              type='text'
+              onChange={this.setUsername}
+            />
+            <button onClick={event => { context.state.updateUsername(this.state.inputValue) }} > send </button>
+          </div>
+        )}
+      </UserConsumer>
+    )
+  }
 }
